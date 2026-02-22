ca5f4d9009d7f76c3b2a47d314c58b612467553a
emmav <emma.vancayseele@gmail.com>
2026-02-22 16:43:52 +0100

    update tests

---
+++ tests/test.py
@@ -0,0 +1,119 @@
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from src.Emulator import Emulator
+from src.ROM_basis import ROM_basis
+from src.ROM_basis import ROM_builder
+
+# test greedy SAMPLER
+# test equidistant SAMPLER
+# test sumrule SAMPLER
+
+# test greedy PREDICTION
+# test equidistant PREDICTION
+# test sumrule PREDICTION
+
+# todo: how to test both ? or not required ?
+
+test_input = Path(__file__).parent.resolve().joinpath('_inputs')
+test_output = Path(__file__).parent.resolve().joinpath('_outputs')
+test_reference = Path(__file__).parent.resolve().joinpath('_references')
+
+class TestSamplers:
+    """
+    Can only be used when tantalus is available.
+    """
+    def test_equidistant(self):
+        WAVE_FUNCTION = '/mnt/c/users/emmav/PycharmProjects/ROM/tests/_inputs/wf.12.12.30.30'
+        MF_OUT = '/mnt/c/users/emmav/PycharmProjects/ROM/tests/_inputs/mf.12.12.30.30.out'
+        FAM_INPUT = {'w_min': 0.0,
+                     'w_max': 40.0,
+                     'num_snapshots': 5,
+                     'smear': 1.0,
+                     'l': 0,  # multipolarity
+                     'm': 0,  # magnetic quantum number
+                     }
+
+        rom_builder = ROM_builder(path_to_meanfield_wf=WAVE_FUNCTION,
+                                  path_to_meanfield_out=MF_OUT,
+                                  FAM=FAM_INPUT,
+                                  tantalus_path='$HOME/code/tantalus/')
+
+        rom_builder.build_type = 'equidistant_1D'
+        rom_builder.output_dir = test_output
+
+        rom_builder.build_snapshot_basis()
+
+        compare_xy_files(rom_builder.path_to_snapshot, reference_path=test_reference.joinpath('emulator_equid.xy'))
+
+
+    def test_greedy(self):
+        WAVE_FUNCTION = '/mnt/c/users/emmav/PycharmProjects/ROM/tests/_inputs/wf.12.12.30.30'
+        MF_OUT = '/mnt/c/users/emmav/PycharmProjects/ROM/tests/_inputs/mf.12.12.30.30.out'
+        FAM_INPUT = {'w_min': 0.0,
+                     'w_max': 40.0,
+                     'num_snapshots': 5,
+                     'smear': 1.0,
+                     'l': 0,  # multipolarity
+                     'm': 0,  # magnetic quantum number
+                     }
+
+        rom_builder = ROM_builder(path_to_meanfield_wf=WAVE_FUNCTION,
+                                  path_to_meanfield_out=MF_OUT,
+                                  FAM=FAM_INPUT,
+                                  tantalus_path='$HOME/code/tantalus/')
+
+        rom_builder.build_type = 'greedy'
+        rom_builder.output_dir = test_output
+
+        rom_builder.build_snapshot_basis()
+
+        compare_xy_files(rom_builder.path_to_snapshot, reference_path=test_reference.joinpath('emulator_greedy.xy'))
+
+
+    def test_contour(self):
+        # TODO
+        # code for contour sampler
+        assert True
+
+class TestPredictions:
+    def test_strength(self):
+        for emul_name in ['emulator_greedy', 'emulator_equid']:
+            e = Emulator(basis=ROM_basis().load(path_to_snapshot=test_input.joinpath(f'{emul_name}.xy')))
+
+            e.projection_method = "G"
+            compare_emulated_strength(emulator=e, reference_emulator_output=test_reference.joinpath(f'{emul_name}_G'))
+
+            e.projection_method = "PG"
+            compare_emulated_strength(emulator=e, reference_emulator_output=test_reference.joinpath(f'{emul_name}_PG'))
+
+    def test_sumrule(self):
+        # TODO
+        assert True
+        # # code for sumrule prediction
+        # compare_m1() # using G
+        # compare_m1() # using PG
+
+
+
+
+
+def compare_xy_files(output_path, reference_path):
+    # todo: make less strict
+    assert output_path.read_text() == reference_path.read_text()
+
+
+def compare_emulated_strength(emulator : Emulator, reference_emulator_output):
+    df = pd.read_csv(
+        reference_emulator_output,
+        converters={"omega": complex}  # This forces the string to be parsed as a complex number
+    )
+    d = df.to_dict(orient="list")
+
+    targets, S = emulator.evaluate(targets= d['omega'])
+    assert np.all(np.isclose(0, S - d['S_tot'], rtol=1e-10))
+
+
+def compare_m1(emulator : Emulator, reference_emulator_output):
+    assert True
