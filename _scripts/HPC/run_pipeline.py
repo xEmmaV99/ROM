04211127f2a3348c7dc2bb6e6238c2f5e7ade22e
xEmmaV99 <emma.vancayseele@gmail.com>
2026-02-23 10:28:33 +0100

    message

--- _scripts/HPC/run_pipeline.py
+++ _scripts/HPC/run_pipeline.py
@@ -1,14 +1,20 @@
 from src.ROM_basis import ROM_builder
 import sys
+# call this: python run_pipeline.py 10 1.0 BSkG2
 
 if __name__ == "__main__":
-    WAVE_FUNCTION = f'/mnt/c/users/emmav/PycharmProjects/ROM/_inputs/wf.12.12.100.100.BSkG2.8'
-    MF_OUT = f'/mnt/c/users/emmav/PycharmProjects/ROM/_inputs/mf.12.12.100.100.BSkG2.8.out'
+    #sys.argv[0] is a path
+    param=str(sys.argv[3])
+    smear=float(sys.argv[2])
+    num_snaps=int(sys.argv[1])
+
+    WAVE_FUNCTION = f'/mnt/c/users/emmav/PycharmProjects/ROM/_inputs/wf.12.12.100.100.{param}.8'
+    MF_OUT = f'/mnt/c/users/emmav/PycharmProjects/ROM/_inputs/mf.12.12.100.100.{param}.8.out'
 
     FAM_INPUT = {'w_min': 0.0,
-                 'w_max': 40.0,
-                 'num_snapshots': int(sys.argv[1]),  # number of snapshots to generate
-                 'smear': 1.0,
+                 'w_max': 50.0,
+                 'num_snapshots': num_snaps,  # number of snapshots to generate
+                 'smear': smear,
                  'l': 0,  # multipolarity
                  'm': 0,  # magnetic quantum number
                  }
