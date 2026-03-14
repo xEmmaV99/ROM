7f66fa732767e2734e6789095392a009142b12f7
xEmmaV99 <emma.vancayseele@gmail.com>
2026-03-14 14:01:40 +0100

    update example scripts

--- example_scripts/Emulator_script.py
+++ example_scripts/Emulator_script.py
@@ -1,44 +1,21 @@
 """
-TODO: fix this script !
+Example script for emulator application
 """
 
-from src.ROM_basis import ROM_builder, ROM_basis
+from src.ROM_basis import ROM_basis
 from src.Emulator import Emulator
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+from pathlib import Path
+CWD = Path(os.getcwd())
 
-WAVE_FUNCTION = '/mnt/c/users/emmav/PycharmProjects/ROM/_input/wf.12.12.30.30'
-MF_OUT = '/mnt/c/users/emmav/PycharmProjects/ROM/_input/mf.12.12.30.30.out'
-FAM_INPUT = {'w_min': 5.0,
-             'w_max': 40.0,
-             'max_num_snapshots' : 12,
-             'smear': 1.0,
-             'l': 2 , # multipolarity
-             'm': 0,  # magnetic quantum number
-             }
-
-rom_builder = ROM_builder(path_to_meanfield_wf=WAVE_FUNCTION,
-                          path_to_meanfield_out=MF_OUT,
-                          FAM=FAM_INPUT)
-
-rom_builder.build_type = 'greedy'
-rom_builder.data.max_num_snapshots = 14 #override
-
-# LOAD if it already exists
-path_to_snapshot = 'C:/users/emmav/PycharmProjects/ROM/_outputs/xy.12.12.30.30.2.0.xy'
-rom_builder.basis.load(path_to_snapshot)
-
-#rom_builder.build_snapshot_basis_static(max_workers=8)
-rom_builder.build_snapshot_basis()
-
-path_to_snapshot = rom_builder.path_to_snapshot
-
-
+# Load emulator basis
+path_to_snapshot = CWD.parent.joinpath('_outputs/xy.12.12.30.30.0.0.BSkG2.16.xy')
 basis = ROM_basis()
 basis.load(path_to_snapshot=path_to_snapshot)
-## OR
-basis = rom_builder.basis
 
+# Create emulator
 emul = Emulator(basis=basis)
 
 smear = 1.0
--- example_scripts/ROM_basis_creation_script.py
+++ example_scripts/ROM_basis_creation_script.py
@@ -1,54 +1,43 @@
 """
-TODO: fix this script !
+Example script for basis creation
 """
+from src.ROM_basis import ROM_builder
+import os
+from pathlib import Path
+CWD = Path(os.getcwd())
 
-from src.ROM_basis import ROM_builder, ROM_basis
-from src.Emulator import Emulator
-import numpy as np
-import matplotlib.pyplot as plt
-
-WAVE_FUNCTION = '/mnt/c/users/emmav/PycharmProjects/ROM/_input/wf.12.12.30.30'
-MF_OUT = '/mnt/c/users/emmav/PycharmProjects/ROM/_input/mf.12.12.30.30.out'
+WAVE_FUNCTION = str(CWD.parent.joinpath('_inputs/wf.12.12.30.30'))
+MF_OUT = str(CWD.parent.joinpath('_inputs/mf.12.12.30.30.out'))
+TANTALUS = '/home/emma/code/tantalus/'
 FAM_INPUT = {'w_min': 5.0,
              'w_max': 40.0,
-             'max_num_snapshots' : 12,
+             'max_num_snapshots' : 10,
              'smear': 1.0,
-             'l': 2 , # multipolarity
+             'l': 0, # multipolarity
              'm': 0,  # magnetic quantum number
              }
 
 rom_builder = ROM_builder(path_to_meanfield_wf=WAVE_FUNCTION,
                           path_to_meanfield_out=MF_OUT,
-                          FAM=FAM_INPUT)
+                          FAM=FAM_INPUT,
+                          tantalus_path=TANTALUS)
 
-rom_builder.build_type = 'greedy'
-rom_builder.data.max_num_snapshots = 14 #override
+rom_builder.build_type = 'equidistant' # 'greedy'
+rom_builder.data.max_num_snapshots = 35 #override
 
 # LOAD if it already exists
-path_to_snapshot = 'C:/users/emmav/PycharmProjects/ROM/_outputs/xy.12.12.30.30.2.0.xy'
-rom_builder.basis.load(path_to_snapshot)
+if rom_builder.build_type == 'greedy':
+    path_to_snapshot = CWD.parent.joinpath('_outputs/xy.12.12.30.30.0.0.BSkG2.16.xy')
+    try:
+        rom_builder.basis.load(path_to_snapshot)
+        print("Loaded existing ROM basis from ", path_to_snapshot)
+    except Exception as e:
+        print("No existing ROM basis found at ", path_to_snapshot, ". Building new basis.")
 
-#rom_builder.build_snapshot_basis_static(max_workers=8)
-rom_builder.build_snapshot_basis()
+    rom_builder.build_snapshot_basis()
+elif rom_builder.build_type == 'equidistant':
+    rom_builder.build_snapshot_basis(max_workers=8)
+else:
+    rom_builder.build_snapshot_basis()
 
-path_to_snapshot = rom_builder.path_to_snapshot
-
-
-basis = ROM_basis()
-basis.load(path_to_snapshot=path_to_snapshot)
-## OR
-basis = rom_builder.basis
-
-emul = Emulator(basis=basis)
-
-smear = 1.0
-targets = np.linspace(5,40,2000)+smear*1j # example target frequencies
-emul.projection_method = "PG"  # or "PG"
-x,y = emul.evaluate(targets=targets)
-
-plt.figure()
-plt.plot(x.real, y)
-
-x, y = emul.evaluate(targets=basis.omegas)
-plt.scatter(x.real, y, marker='x')
-plt.show()
+print("ROM basis saved at ", rom_builder.path_to_snapshot)
