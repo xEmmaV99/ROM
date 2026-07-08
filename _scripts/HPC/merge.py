32ff961498e284868daaad027e91fb4bf082e17d
emmav <emma.vancayseele@gmail.com>
2026-07-08 15:45:45 +0200

    cleanup

--- _scripts/HPC/merge.py
+++
@@ -1,9 +0,0 @@
-from src.Utils_parsers import merge_FAM_outputs
-from pathlib import Path
-import sys
-
-if __name__ == "__main__":
-
-
-    # folder = f'/gpfs/home/acad/ulb-iaa/pdemol/code/ROM/src/work_dir/TMP_SNAPSHOTS/'
-    merge_FAM_outputs(folder=sys.argv[1], output_dir=Path(sys.argv[2]))
